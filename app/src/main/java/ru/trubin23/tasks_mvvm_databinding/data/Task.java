452570cc0a6ff9e6d1a5f08c7ad639d9d4e6f675
Andrey Trubin <trubin23@gmail.com>
2018-03-12 11:12:46 +0500

    start the implementation of the Task for Database

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/data/Task.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/data/Task.java
@@ -1,8 +1,49 @@
 package ru.trubin23.tasks_mvvm_databinding.data;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
 /**
  * Created by Andrey on 12.03.2018.
  */
 
-public class Task {
+@Entity(tableName = "tasks")
+public final class Task {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "task-id")
+    private final String mTaskId;
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    private final String mTitle;
+
+    @NonNull
+    @ColumnInfo(name = "description")
+    private final String mDescription;
+
+    @NonNull
+    @ColumnInfo(name = "date-of-creation")
+    private final String mDateOfCreation;
+
+    @ColumnInfo(name = "completed")
+    private final boolean mCompleted;
+
+    @NonNull
+    @ColumnInfo(name = "date-of-change")
+    private final String mDateOfChange;
+
+    public Task(@NonNull String title, @NonNull String description,
+                @NonNull String taskId, @NonNull String dateOfCreation,
+                boolean completed, @NonNull String dateOfChange) {
+        mTitle = title;
+        mDescription = description;
+        mTaskId = taskId;
+        mDateOfCreation = dateOfCreation;
+        mCompleted = completed;
+        mDateOfChange = dateOfChange;
+    }
 }
---
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/data/source/local/TasksDatabase.java
@@ -0,0 +1,11 @@
+package ru.trubin23.tasks_mvvm_databinding.data.source.local;
+
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by Andrey on 12.03.2018.
+ */
+
+
+public abstract class TasksDatabase extends RoomDatabase {
+}
