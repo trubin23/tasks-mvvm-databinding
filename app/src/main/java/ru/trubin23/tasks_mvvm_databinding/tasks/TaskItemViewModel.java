9c31f9512bc7f3a3b84b734bd26bf73f62e116d8
Andrey Trubin <trubin23@gmail.com>
2018-03-17 14:58:57 +0500

    adding methods for communication TaskItemViewModel and task_item.xml

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/tasks/TaskItemViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/tasks/TaskItemViewModel.java
@@ -2,9 +2,11 @@ package ru.trubin23.tasks_mvvm_databinding.tasks;
 
 import android.content.Context;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 
+import ru.trubin23.tasks_mvvm_databinding.R;
 import ru.trubin23.tasks_mvvm_databinding.data.Task;
 import ru.trubin23.tasks_mvvm_databinding.data.source.TasksRepository;
 
@@ -26,5 +28,24 @@ public class TaskItemViewModel extends BaseObservable {
 
     void setTask(@NonNull Task task) {
         mTaskObservable.set(task);
+    }
+
+    public void taskClicked() {
+
+    }
+
+    @Bindable
+    public boolean getCompleted() {
+        Task task = mTaskObservable.get();
+        return task != null && task.isCompleted();
+    }
+
+    @Bindable
+    public String getTitleForItem() {
+        Task task = mTaskObservable.get();
+        if (task == null){
+            return mContext.getString(R.string.no_data);
+        }
+        return task.getTitle();
     }
 }
