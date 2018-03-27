ae4a6aafe5522541a393386d775e0d5fb8d9f11e
Andrey Trubin <trubin23@gmail.com>
2018-03-27 23:56:50 +0500

    start implementation layout for TaskDetailFragment

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/taskdetail/TaskDetailFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/taskdetail/TaskDetailFragment.java
@@ -8,7 +8,15 @@ import android.support.v4.app.Fragment;
 
 public class TaskDetailFragment extends Fragment {
 
+    private static final String ARGUMENT_TASK_ID = "TASK_ID";
+
     private TaskDetailViewModel mViewModel;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mViewModel.start(getArguments().getString(ARGUMENT_TASK_ID));
+    }
 
     public void setViewModel(TaskDetailViewModel viewModel) {
         mViewModel = viewModel;
--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/taskdetail/TaskDetailViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/taskdetail/TaskDetailViewModel.java
@@ -3,7 +3,10 @@ package ru.trubin23.tasks_mvvm_databinding.taskdetail;
 import android.content.Context;
 import android.databinding.BaseObservable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import ru.trubin23.tasks_mvvm_databinding.data.Task;
+import ru.trubin23.tasks_mvvm_databinding.data.source.TasksDataSource;
 import ru.trubin23.tasks_mvvm_databinding.data.source.TasksRepository;
 
 /**
@@ -19,5 +22,21 @@ public class TaskDetailViewModel extends BaseObservable {
     TaskDetailViewModel(@NonNull TasksRepository repository, @NonNull Context context) {
         mTasksRepository = repository;
         mContext = context;
+    }
+
+    void start(@Nullable String taskId) {
+        if (taskId != null){
+            mTasksRepository.getTask(taskId, new TasksDataSource.GetTaskCallback() {
+                @Override
+                public void onTaskLoaded(@NonNull Task task) {
+
+                }
+
+                @Override
+                public void onDataNotAvailable() {
+
+                }
+            });
+        }
     }
 }
