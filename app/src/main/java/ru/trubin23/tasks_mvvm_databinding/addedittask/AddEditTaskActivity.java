673761d6e5a912a012188bd0dabb94f59b53f87e
Trubin Andrey <trubin23@gmail.com>
2018-08-01 23:58:05 +0500

    refactoring code, rename layouts

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskActivity.java
@@ -1,13 +1,15 @@
 package ru.trubin23.tasks_mvvm_databinding.addedittask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.trubin23.tasks_mvvm_databinding.R;
 
 public class AddEditTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_edit_task);
+        setContentView(R.layout.add_edit_task_act);
     }
 }
--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsActivity.java
@@ -1,13 +1,15 @@
 package ru.trubin23.tasks_mvvm_databinding.statistics;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.trubin23.tasks_mvvm_databinding.R;
 
 public class StatisticsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_statistics);
+        setContentView(R.layout.statistics_act);
     }
 }
