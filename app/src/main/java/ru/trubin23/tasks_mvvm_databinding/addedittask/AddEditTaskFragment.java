27f342e1fd9647c4178459e00c392cc724f86672
Trubin Andrey <trubin23@gmail.com>
2018-08-02 23:23:40 +0500

    added layouts for Fragments

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskFragment.java
@@ -3,4 +3,11 @@ package ru.trubin23.tasks_mvvm_databinding.addedittask;
 import android.support.v4.app.Fragment;
 
 public class AddEditTaskFragment extends Fragment {
+
+    public AddEditTaskFragment() {
+    }
+
+    public static AddEditTaskFragment newInstance() {
+        return new AddEditTaskFragment();
+    }
 }
--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsFragment.java
@@ -3,4 +3,11 @@ package ru.trubin23.tasks_mvvm_databinding.statistics;
 import android.support.v4.app.Fragment;
 
 public class StatisticsFragment extends Fragment {
+
+    public StatisticsFragment() {
+    }
+
+    public static StatisticsFragment newInstance() {
+        return new StatisticsFragment();
+    }
 }
