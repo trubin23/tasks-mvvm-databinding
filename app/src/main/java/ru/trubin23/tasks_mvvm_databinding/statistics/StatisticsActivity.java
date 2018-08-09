6939be69915a073e68c585c46bfb92a004176467
Trubin Andrey <trubin23@gmail.com>
2018-08-09 23:34:25 +0500

    added Toolbar to StatisticsActivity

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsActivity.java
@@ -1,7 +1,9 @@
 package ru.trubin23.tasks_mvvm_databinding.statistics;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import ru.trubin23.tasks_mvvm_databinding.R;
 
@@ -11,5 +13,18 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_act);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.statistics);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
