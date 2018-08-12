5e842b93944a044135f005cae2e932dff3dfc571
Trubin Andrey <trubin23@gmail.com>
2018-08-12 23:37:57 +0500

    added ActionBar in AddEditTaskActivity

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskActivity.java
@@ -1,7 +1,9 @@
 package ru.trubin23.tasks_mvvm_databinding.addedittask;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import ru.trubin23.tasks_mvvm_databinding.R;
 
@@ -11,5 +13,17 @@ public class AddEditTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit_task_act);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
