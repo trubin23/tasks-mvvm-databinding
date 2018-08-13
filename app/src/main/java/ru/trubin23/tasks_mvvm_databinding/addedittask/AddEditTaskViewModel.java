9440b13a244496be5d05e235765d53aa41501795
Trubin Andrey <trubin23@gmail.com>
2018-08-13 22:22:46 +0500

    continue filling the layout add_edit_task_frag

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskViewModel.java
@@ -1,4 +1,10 @@
 package ru.trubin23.tasks_mvvm_databinding.addedittask;
 
+import android.databinding.ObservableBoolean;
+
 public class AddEditTaskViewModel {
+
+
+    public final ObservableBoolean mDataLoading = new ObservableBoolean(false);
+
 }
