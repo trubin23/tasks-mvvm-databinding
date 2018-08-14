89c98121bfc35e7468dfc4b2412dfc6f89591a61
Trubin Andrey <trubin23@gmail.com>
2018-08-14 23:58:51 +0500

    the implementation of the menu in TaskDetailFragment is added

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/taskdetail/TaskDetailFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/taskdetail/TaskDetailFragment.java
@@ -2,6 +2,11 @@ package ru.trubin23.tasks_mvvm_databinding.taskdetail;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import ru.trubin23.tasks_mvvm_databinding.R;
 
 /**
  * Created by Andrey on 20.03.2018.
@@ -28,6 +33,20 @@ public class TaskDetailFragment extends Fragment {
     public void onResume() {
         super.onResume();
         mViewModel.start(getArguments().getString(ARGUMENT_TASK_ID));
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.taskdetail_frag_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_delete:
+                return true;
+        }
+        return false;
     }
 
     public void setViewModel(TaskDetailViewModel viewModel) {
