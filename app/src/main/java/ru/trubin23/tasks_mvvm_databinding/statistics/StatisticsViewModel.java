6bf029f77286315327f36b70d135c61b0ffd089f
Trubin Andrey <trubin23@gmail.com>
2018-08-17 23:57:58 +0500

    Start of StatisticsViewModel implementation

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsViewModel.java
@@ -1,4 +1,18 @@
 package ru.trubin23.tasks_mvvm_databinding.statistics;
 
+import android.databinding.Bindable;
+import android.databinding.ObservableBoolean;
+
 public class StatisticsViewModel {
+
+    public final ObservableBoolean mDataLoading = new ObservableBoolean(false);
+
+    private int mNumberOfActiveTasks = 0;
+
+    private int mNumberOfCompletedTasks = 0;
+
+    @Bindable
+    public boolean isEmpty() {
+        return mNumberOfActiveTasks + mNumberOfCompletedTasks == 0
+    }
 }
