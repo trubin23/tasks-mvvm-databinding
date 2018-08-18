4d1f3fdef8b2cb350038bbf781929ee818a8577a
Trubin Andrey <trubin23@gmail.com>
2018-08-18 22:44:20 +0500

    continue of StatisticsViewModel implementation

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsViewModel.java
@@ -1,7 +1,15 @@
 package ru.trubin23.tasks_mvvm_databinding.statistics;
 
+import android.content.Context;
 import android.databinding.Bindable;
 import android.databinding.ObservableBoolean;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import ru.trubin23.tasks_mvvm_databinding.data.Task;
+import ru.trubin23.tasks_mvvm_databinding.data.source.TasksDataSource;
+import ru.trubin23.tasks_mvvm_databinding.data.source.TasksRepository;
 
 public class StatisticsViewModel {
 
@@ -11,8 +19,35 @@ public class StatisticsViewModel {
 
     private int mNumberOfCompletedTasks = 0;
 
+    private Context mContext;
+
+    private final TasksRepository mTasksRepository;
+
+    private StatisticsViewModel(Context context, TasksRepository tasksRepository){
+        mContext = context;
+        mTasksRepository = tasksRepository;
+    }
+
+    public void start() {
+        mDataLoading.set(true);
+
+        mTasksRepository.getTasks(new TasksDataSource.LoadTasksCallback() {
+            @Override
+            public void onTasksLoaded(@NonNull List<Task> tasks) {
+
+
+                mDataLoading.set(false);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                mDataLoading.set(false);
+            }
+        });
+    }
+
     @Bindable
     public boolean isEmpty() {
-        return mNumberOfActiveTasks + mNumberOfCompletedTasks == 0
+        return mNumberOfActiveTasks + mNumberOfCompletedTasks == 0;
     }
 }
