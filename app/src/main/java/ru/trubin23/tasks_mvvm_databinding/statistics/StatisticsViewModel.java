f88e505d74349c8712f9467b5c486f3252aced61
Trubin Andrey <trubin23@gmail.com>
2018-08-18 22:56:41 +0500

    finish of StatisticsViewModel implementation

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsViewModel.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 
 import java.util.List;
 
+import ru.trubin23.tasks_mvvm_databinding.R;
 import ru.trubin23.tasks_mvvm_databinding.data.Task;
 import ru.trubin23.tasks_mvvm_databinding.data.source.TasksDataSource;
 import ru.trubin23.tasks_mvvm_databinding.data.source.TasksRepository;
@@ -23,7 +24,7 @@ public class StatisticsViewModel {
 
     private final TasksRepository mTasksRepository;
 
-    private StatisticsViewModel(Context context, TasksRepository tasksRepository){
+    private StatisticsViewModel(Context context, TasksRepository tasksRepository) {
         mContext = context;
         mTasksRepository = tasksRepository;
     }
@@ -34,7 +35,19 @@ public class StatisticsViewModel {
         mTasksRepository.getTasks(new TasksDataSource.LoadTasksCallback() {
             @Override
             public void onTasksLoaded(@NonNull List<Task> tasks) {
+                int active = 0;
+                int completed = 0;
 
+                for (Task task : tasks) {
+                    if (task.isCompleted()) {
+                        completed++;
+                    } else {
+                        active++;
+                    }
+                }
+
+                mNumberOfActiveTasks = active;
+                mNumberOfCompletedTasks = completed;
 
                 mDataLoading.set(false);
             }
@@ -49,5 +62,15 @@ public class StatisticsViewModel {
     @Bindable
     public boolean isEmpty() {
         return mNumberOfActiveTasks + mNumberOfCompletedTasks == 0;
+    }
+
+    @Bindable
+    public String getNumberOfActiveTasks() {
+        return mContext.getString(R.string.statistics_active_tasks, mNumberOfActiveTasks);
+    }
+
+    @Bindable
+    public String getNumberOfCompletedTasks() {
+        return mContext.getString(R.string.statistics_completed_tasks, mNumberOfCompletedTasks);
     }
 }
