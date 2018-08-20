cfbb64b0e3b9df7683670a63a550f4f63ff2cfec
Trubin Andrey <trubin23@gmail.com>
2018-08-20 23:56:17 +0500

    added function findOrCreateViewModel in AddEditTaskActivity

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskActivity.java
@@ -6,13 +6,18 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import ru.trubin23.tasks_mvvm_databinding.Injection;
 import ru.trubin23.tasks_mvvm_databinding.R;
+import ru.trubin23.tasks_mvvm_databinding.ViewModelHolder;
 import ru.trubin23.tasks_mvvm_databinding.taskdetail.TaskDetailFragment;
+import ru.trubin23.tasks_mvvm_databinding.taskdetail.TaskDetailViewModel;
 import ru.trubin23.tasks_mvvm_databinding.util.ActivityUtils;
 
 public class AddEditTaskActivity extends AppCompatActivity {
 
     public static final String ADD_EDIT_TASK_ID = "ADD_EDIT_TASK_ID";
+
+    private static final String ADDEDITTASK_VIEWMODEL_TAG = "ADDEDITTASK_VIEWMODEL_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,5 +55,28 @@ public class AddEditTaskActivity extends AppCompatActivity {
                     addEditTaskFragment, R.id.content_frame);
         }
         return addEditTaskFragment;
+    }
+
+    @NonNull
+    private AddEditTaskViewModel findOrCreateViewModel() {
+        //noinspection unchecked
+        ViewModelHolder<AddEditTaskViewModel> retainedViewModel =
+                (ViewModelHolder<AddEditTaskViewModel>) getSupportFragmentManager()
+                        .findFragmentByTag(ADDEDITTASK_VIEWMODEL_TAG);
+
+        if (retainedViewModel != null && retainedViewModel.getViewModel() != null) {
+            return retainedViewModel.getViewModel();
+        } else {
+            AddEditTaskViewModel viewModel = new AddEditTaskViewModel(
+                    Injection.provideTasksRepository(getApplicationContext()),
+                    getApplicationContext()
+            );
+
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
+                    ViewModelHolder.createContainer(viewModel),
+                    ADDEDITTASK_VIEWMODEL_TAG);
+
+            return viewModel;
+        }
     }
 }
--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskViewModel.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/addedittask/AddEditTaskViewModel.java
@@ -1,9 +1,14 @@
 package ru.trubin23.tasks_mvvm_databinding.addedittask;
 
+import android.content.Context;
+import android.databinding.BaseObservable;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
+import android.support.annotation.NonNull;
 
-public class AddEditTaskViewModel {
+import ru.trubin23.tasks_mvvm_databinding.data.source.TasksRepository;
+
+public class AddEditTaskViewModel extends BaseObservable {
 
     public final ObservableField<String> mTitle = new ObservableField<>();
 
@@ -11,4 +16,6 @@ public class AddEditTaskViewModel {
 
     public final ObservableBoolean mDataLoading = new ObservableBoolean(false);
 
+    AddEditTaskViewModel(@NonNull TasksRepository repository, @NonNull Context context) {
+    }
 }
