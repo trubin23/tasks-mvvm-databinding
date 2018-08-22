1828e677678dcde42de0a96d310db07c7a9efcfe
Trubin Andrey <trubin23@gmail.com>
2018-08-22 23:53:58 +0500

    added view to StatisticsFragment

--- app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvvm_databinding/statistics/StatisticsFragment.java
@@ -1,7 +1,14 @@
 package ru.trubin23.tasks_mvvm_databinding.statistics;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import ru.trubin23.tasks_mvvm_databinding.R;
 import ru.trubin23.tasks_mvvm_databinding.databinding.StatisticsFragBinding;
 
 public class StatisticsFragment extends Fragment {
@@ -11,6 +18,20 @@ public class StatisticsFragment extends Fragment {
     private StatisticsFragBinding mStatisticsFragBinding;
 
     public StatisticsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mStatisticsFragBinding = DataBindingUtil.inflate(
+                inflater, R.layout.statistics_frag, container, false);
+        return mStatisticsFragBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mStatisticsFragBinding.setViewModel(mViewModel);
     }
 
     public static StatisticsFragment newInstance() {
